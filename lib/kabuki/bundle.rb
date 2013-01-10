387d283e1b114b8155239f27e60b7306e333b19f
macintosh <macintosh@obama.local>
2013-01-10 17:18:20 +0400

    initial commit

--- lib/kabuki/bundle.rb
+++ lib/kabuki/bundle.rb
@@ -5,12 +5,12 @@ end
 
 class Object
   def kabuki
-    self.kabuki_dump.kabuki_zip.kabuki_encrypt
+    Base64.strict_encode64(self.kabuki_dump.kabuki_zip.kabuki_encrypt)
   end
 end
 
 class String
   def kabuki
-    self.kabuki_decrypt.kabuki_unzip.kabuki_load
+    Base64.strict_decode64(self).kabuki_decrypt.kabuki_unzip.kabuki_load
   end
 end
