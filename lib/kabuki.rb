be35a3ce8dc4b6580f12a0a247d1ab7d012fef90
Valery Kvon <addagger@gmail.com>
2013-01-25 22:41:37 +0400

    moved to "active_tools"

--- lib/kabuki.rb
+++ lib/kabuki.rb
@@ -4,6 +4,14 @@ module Kabuki
   def self.load!
     require 'kabuki/engine'
     require 'kabuki/railtie'
+    warn <<-EOW
+
+    DEPRECATION WARNING: Gem 'kabuki' has been moved into 'active_tools' gem.
+    Further development of 'kabuki' will no longer continue.
+
+    Thank you!
+
+EOW
   end
 end
 
